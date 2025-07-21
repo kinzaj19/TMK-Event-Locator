38fa5bb50843bc6be2b499d19893f766d0353a7a
Zia Rozmin <zia.rozmin@gmail.com>
2025-07-20 20:27:27 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`✅ TMK Backend running on http://localhost:${PORT}`);
+});
+//Set up server starter logic in server.js
 
