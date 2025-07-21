e4a3302c9ebbaeff27fea8eaffed3c94e10ce43c
kinzaj19 <kinza.jawaid19@gmail.com>
2025-07-22 00:47:30 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import EventDashboard from './pages/EventDashboard';
+import './index.css';
 
+function App() {
+  return (
+    <div className="App">
+      <EventDashboard />
+    </div>
+  );
+}
+
+export default App;
